c736bf8aa5cb376942126e3c6d6b6dc91ec27362
NodjaLima <nodjamplima@gmail.com>
2022-09-26 16:29:46 -0300

    criado bando de dados mockado para inserir lógica dinamica de tabela

---
+++ data/data.js
@@ -0,0 +1,28 @@
+const Data = [
+  {
+    id: 1,
+    name: 'Pedro Silva',
+    class: 'T10',
+    age: 19
+  },
+  {
+    id: 2,
+    name: 'Debora Lopes',
+    class: 'T10',
+    age: 25
+  },
+  {
+    id: 3,
+    name: 'Barbara Ruiz',
+    class: 'T4',
+    age: 22
+  },
+  {
+    id: 4,
+    name: 'Vitor Vasconcelos',
+    class: 'T4',
+    age: 30
+  }
+]
+ 
+export default Data;
